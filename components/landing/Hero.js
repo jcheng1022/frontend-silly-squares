b76d45ef559dc37c006bc13ae24074815f77a4ac
Jacky <87586598+jcheng1022@users.noreply.github.com>
2024-09-09 13:37:17 -0400

    update hero cta

--- components/landing/Hero.js
+++ components/landing/Hero.js
@@ -3,10 +3,22 @@
 import React from 'react';
 import {useRouter} from "next/navigation";
 import {useAuthContext} from "@/context/AuthContext";
+import {useCurrentUser} from "@/hooks/user.hooks";
 
 const Hero = () => {
     const router = useRouter()
     const { handleSignIn} = useAuthContext()
+    const {data: user, isLoading} = useCurrentUser()
+
+    const handleCta = () => {
+        if (isLoading) return;
+
+        if (!!user) {
+            router.push('/generate')
+        } else {
+            handleSignIn()
+        }
+    }
     return (
         <div className={'py-12 text-center'}>
 
@@ -14,7 +26,7 @@ const Hero = () => {
                 <div className={' text-2xl sm:text-6xl font-bold'}> Code Tests Generator</div>
                 <div className={'text-2xl text-slate-400'}> No more untested code </div>
             </div>
-            <button onClick={() => handleSignIn()} className={' text-2xl font-bold rounded-xl bg-orange-400 px-8 py-4'}> Get Started</button>
+            <button onClick={handleCta} className={' text-2xl font-bold rounded-xl bg-orange-400 px-8 py-4'}> Get Started</button>
 
 
         </div>
